2f463dd62952a311b0e7ac5d0029f837b1b00457
shivani1012 <Shivanipanchal1012@gmail.com>
2023-06-28 11:45:15 +0530

    code structured

---
+++ src/component/Dashboard/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { employeeData } from '../../data/data';
+
+const Dashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/data/data.js
@@ -0,0 +1,100 @@
+const employeeData = [
+    {
+        id: 1,
+        firstName: 'Shivani',
+        lastName: 'Panchal',
+        email: 'shivani@company.com',
+        salary: '20000',
+        date: '2020-05-11'
+    },
+    {
+        id: 2,
+        firstName: 'Priyanka',
+        lastName: 'Panchal',
+        email: 'priyanka@company.com',
+        salary: '25000',
+        date: '2021-09-14'
+    },
+    {
+        id: 3,
+        firstName: 'Jinal',
+        lastName: 'Panchal',
+        email: 'jinal@company.com',
+        salary: '23000',
+        date: '2021-03-20'
+    },
+    {
+        id: 4,
+        firstName: 'Harshil',
+        lastName: 'Panchal',
+        email: 'harshil@company.com',
+        salary: '80000',
+        date: '2022-11-20'
+    },
+    {
+        id: 5,
+        firstName: 'Mayur',
+        lastName: 'Panchal',
+        email: 'mayur@company.com',
+        salary: '90000',
+        date: '2020-05-11'
+    },
+    {
+        id: 6,
+        firstName: 'Dixit',
+        lastName: 'Panchal',
+        email: 'dixit@company.com',
+        salary: '28000',
+        date: '2021-11-26'
+    },
+    {
+        id: 7,
+        firstName: 'Jigna',
+        lastName: 'Panchal',
+        email: 'jigna@company.com',
+        salary: '23000',
+        date: '2022-06-24'
+    },
+    {
+        id: 8,
+        firstName: 'Divyata',
+        lastName: 'Shah',
+        email: 'divyata@company.com',
+        salary: '40000',
+        date: '2022-12-29'
+    },
+    {
+        id: 9,
+        firstName: 'Somil',
+        lastName: 'Panchal',
+        email: 'somil@company.com',
+        salary: '30000',
+        date: '2021-10-22'
+    },
+    {
+        id: 10,
+        firstName: 'Biren',
+        lastName: 'Shah',
+        email: 'biren@company.com',
+        salary: '42000',
+        date: '2022-02-28'
+    },
+    {
+        id: 11,
+        firstName: 'Vihaa',
+        lastName: 'Shah',
+        email: 'vihaa@company.com',
+        salary: '50000',
+        date: '2021-07-12'
+    },
+    {
+        id: 12,
+        firstName: 'Veer',
+        lastName: 'Shah',
+        email: 'veer@company.com',
+        salary: '85000',
+        date: '2022-04-21'
+    },
+]
+
+export { employeeData }
