ffac1f87c92f433e0e591a4a2962f52cbaca9300
shivani1012 <Shivanipanchal1012@gmail.com>
2023-07-11 15:01:03 +0530

    List page data structured

--- src/component/Dashboard/List.js
+++ src/component/Dashboard/List.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
 
-const List = () => {
+const List = ({employees, handleEdit, handleDelete}) => {
+  
   return (
-    <div>
-      List Page
+    <div className='contain-table'>
+      <table className='striped-table'>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Salary</th>
+            <th>Date</th>
+            <th colSpan={2} className='text-center'>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.length > 0 ? (
+            employees.map((employee, i) => {
+              <tr key={employee.id}>
+                <td>{i + 1}</td>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.email}</td>
+                <td>{employee.salary}</td>
+                <td>{employee.date}</td>
+                <td className='text-right'>
+                  <button 
+                    className='white-button'
+                    onClick={() => handleEdit(employee.id)}
+                  >Edit</button>
+                </td>
+                <td className='text-left'>
+                  <button 
+                    className='danger-button'
+                    onClick={() => handleDelete(employee.id)}
+                  >Delete</button>
+                </td>
+              </tr>
+            })
+          ) : (
+            <tr>
+              <td colSpan={7}>No Employees!</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
